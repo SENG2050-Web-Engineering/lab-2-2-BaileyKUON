15ab4591b4d017b9fb82e78dfdd5d2cc86df9611
BaileyKUON <c3405660@uon.edu.au>
2026-02-09 15:06:57 +1100

    Task 4.1 - Added Form Submission and Post Method

--- lab2/src/main/java/seng2050/lab2/GreetingServlet.java
+++ lab2/src/main/java/seng2050/lab2/GreetingServlet.java
@@ -33,4 +33,29 @@ public class GreetingServlet extends HttpServlet {
         out.println("</html>");
 
     }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        String firstName = "";
+        firstName = request.getParameter("firstName");
+        String lastName = "";
+        lastName = request.getParameter("lastName");
+
+        PrintWriter out = resp.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html lang>");
+        out.println("<head> <title> Greeting Servlet </title> </head>");
+        out.println("<body>");
+        out.println("<h1>Greeting Servlet</h1>");
+        if (lastName.matches(".*\\d.*") || lastName == "" || firstName.matches(".*\\d.*") || firstName == "") {
+            out.println("<h2> Your first name and/or last name cannot be null or contain numbers e.g. (0-9)</h2>");
+        } else {
+            out.println("<h2>Welcome " + firstName + " " + lastName
+                    + " to the greeting servlet where your name will be displayed with a welcome message.</h2>");
+        }
+        out.println("</body>");
+        out.println("</html>");
+
+    }
 }
